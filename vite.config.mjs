fb61464a635acd96be0960fde8999513ac290103
jasonjlocicero-cyber <jasonjlocicero@gmail.com>
2025-09-19 21:08:26 -0400

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,4 @@
+// vite.config.mjs  (delete vite.config.js if it exists)
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig({ plugins: [react()] })
