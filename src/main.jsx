dbdbdb229aaeb7ff017fc179aa79bfba9568722d
jasonjlocicero <jasonjlocicero@gmail.com>
2025-10-01 14:10:15 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+// src/main.jsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
 import App from './App'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+const rootEl = document.getElementById('root')
+createRoot(rootEl).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
 
 
