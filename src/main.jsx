49b5385983f422ed9eb1f487c090da212201d15c
jasonjlocicero <jasonjlocicero@gmail.com>
2025-11-27 13:59:42 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
+
+// …existing imports/ReactDOM render stay as-is
 
 const rootEl = document.getElementById('root')
 createRoot(rootEl).render(
