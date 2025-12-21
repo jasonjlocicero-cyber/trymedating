a9841c1ad56d853d1190d6c0b5be246c09d6768f
Jason Locicero <jasonjlocicero@gmail.com>
2025-12-21 14:02:54 -0500

    Add missing src/pwa maybeRegisterSW helper

---
+++ src/pwa/maybeRegisterSW.js
@@ -0,0 +1,18 @@
+// src/pwa/maybeRegisterSW.js
+export default async function maybeRegisterSW() {
+  try {
+    const mod = await import('virtual:pwa-register')
+    const registerSW = mod?.registerSW
+    if (typeof registerSW !== 'function') return
+
+    registerSW({
+      immediate: true,
+      onRegistered() {},
+      onRegisterError(err) {
+        console.warn('[PWA] SW register error:', err)
+      }
+    })
+  } catch (err) {
+    console.warn('[PWA] SW setup skipped:', err)
+  }
+}
