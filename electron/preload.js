6d3dfe1354d3c9f65761f7a725a28c063b679725
jasonjlocicero <jasonjlocicero@gmail.com>
2025-12-20 20:14:43 -0500

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,9 +1,17 @@
-﻿// electron/preload.js (CommonJS)
+// electron/preload.js (CommonJS)
+'use strict'
 
-const { contextBridge } = require("electron");
+const { contextBridge } = require('electron')
 
-contextBridge.exposeInMainWorld("tmd", {
+// Values are set by electron/main.js before creating the BrowserWindow
+const env = process.env.TMD_ENV || 'unknown'
+const devToolsAllowed = (process.env.TMD_DEVTOOLS_ALLOWED || '0') === '1'
+
+// Keep the surface area small + immutable
+const tmdApi = Object.freeze({
   isElectron: true,
-  env: process.env.TMD_ENV || "unknown",
-  devToolsAllowed: (process.env.TMD_DEVTOOLS_ALLOWED || "0") === "1"
-});
+  env,
+  devToolsAllowed
+})
+
+contextBridge.exposeInMainWorld('tmd', tmdApi)
